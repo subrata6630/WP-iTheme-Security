ee94dac25bf972d288e1ccbb15c310919061a70c
Tang Rufus <tangrufus@gmail.com>
2017-03-10 16:18:01 +0800

    wp scaffold plugin wpcfg-ithemes-security

---
+++ wpcfg-ithemes-security.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wpcfg Ithemes Security
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpcfg-ithemes-security
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpcfg_Ithemes_Security
+ */
